f3980a7d849a55cb84be824c351c20b61de93a28
= <jhrabe@wisc.edu>
2022-05-20 14:49:55 -0400

    add admissions page + route - admissions page styling needs some fixing to move elements left - the list-group of schools could also use tidying up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
+  get '/admissions', to: 'static_pages#admissions'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,13 @@
-require "test_helper"
+require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get about" do
+  test 'should get about' do
     get about_url
+    assert_response :success
+  end
+
+  test 'should get admissions' do
+    get admissions_url
     assert_response :success
   end
 end
